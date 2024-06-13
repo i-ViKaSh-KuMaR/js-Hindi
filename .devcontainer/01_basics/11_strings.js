46611ad1c79e96f825fc7c66e10dab03b79e910f
i-ViKaSh-KuMaR <137139377+i-ViKaSh-KuMaR@users.noreply.github.com>
2024-06-13 12:21:01 +0000

    strings

---
+++ .devcontainer/01_basics/11_strings.js
@@ -0,0 +1,31 @@
+const name = "Vikash"
+const repoCount = 50
+
+
+// console.log(name + repoCount + "value")
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+const gameName = new String('vikashk')
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.toUpperCase);
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+const anotherString = gameName.slice(0,4)
+console.log(anotherString);
+
+const newStringOne="  vikash   "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+console.log(url.replace('%20','_'))
+
+console.log(url.includes('sundar'));
+
+
