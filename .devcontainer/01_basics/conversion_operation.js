251c6069cf2099b2c72e4d04eb6518f85f91575f
i-ViKaSh-KuMaR <137139377+i-ViKaSh-KuMaR@users.noreply.github.com>
2024-06-13 09:52:07 +0000

    operation

---
+++ .devcontainer/01_basics/conversion_operation.js
@@ -0,0 +1,9 @@
+let value=3;
+let negValue=-value;
+// console.log(negValue);
+let str1="viKASH";
+let str2=" kumar";
+let str3=str1+str2;
+console.log(str3);
+console.log("2"+3);
+console.log(2+"3");
